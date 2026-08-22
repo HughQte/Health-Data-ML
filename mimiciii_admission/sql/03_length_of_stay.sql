33da220f2b948e302da79c9f5f25d92f0c62ba42
Hieu Bui <107234354+HughQte@users.noreply.github.com>
2026-08-22 02:43:31 -0500

    Add query to calculate average length of stay by type

--- mimiciii_admission/sql/03_length_of_stay.sql
+++ mimiciii_admission/sql/03_length_of_stay.sql
@@ -12,3 +12,18 @@ SELECT
 
 FROM mimiciii_admissions
 ORDER BY los_days DESC;
+
+SELECT
+    admission_type,
+    COUNT(*) AS admissions,
+
+    ROUND(
+        AVG(
+            TIMESTAMPDIFF(HOUR, admittime, dischtime) / 24.0
+        ),
+        2
+    ) AS avg_los_days
+
+FROM mimiciii_admissions
+GROUP BY admission_type
+ORDER BY avg_los_days DESC;
